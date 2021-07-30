735eac86dfdb23e7409a8a88628f6df1d8eac21e
msmaker <marcosilva.tec@gmail.com>
2021-07-30 01:56:36 -0300

    Tratamento de campo não preenchido

--- src/main/java/com/msmaker/os/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/msmaker/os/resources/exceptions/ResourceExceptionHandler.java
@@ -2,6 +2,8 @@ package com.msmaker.os.resources.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -23,6 +25,18 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> objectNotFoundException(DataIntegrityViolationException e) {
 		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
 				e.getMessage());
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> objectNotFoundException(MethodArgumentNotValidException e) {
+		ValidationError error = new ValidationError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+				"Erro na validação dos campos!");
+		
+		for(FieldError x : e.getBindingResult().getFieldErrors()) {
+			error.addError(null, x.getDefaultMessage());
+		}
 
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
