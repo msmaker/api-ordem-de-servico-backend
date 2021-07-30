f4e8198fd97d3177e65bb717f014a6c0ce815080
msmaker <marcosilva.tec@gmail.com>
2021-07-29 21:54:02 -0300

    Implementando Tecnico Resource/COntroller endopoint findById

---
+++ src/main/java/com/msmaker/os/service/TecnicoService.java
@@ -0,0 +1,21 @@
+package com.msmaker.os.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.msmaker.os.domain.Tecnico;
+import com.msmaker.os.repositories.TecnicoRepository;
+
+@Service
+public class TecnicoService {
+
+	@Autowired
+	private TecnicoRepository repository;
+	
+	public Tecnico findById(Integer id) {
+		Optional<Tecnico> obj = repository.findById(id);
+		return  obj.orElse(null);
+	}
+}
