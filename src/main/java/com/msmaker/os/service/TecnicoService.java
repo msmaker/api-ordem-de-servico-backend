2b63c680b1b836cb1e14e3d0f52a9af6ed200f4c
msmaker <marcosilva.tec@gmail.com>
2021-07-29 23:03:50 -0300

    Error Handler

--- src/main/java/com/msmaker/os/service/TecnicoService.java
+++ src/main/java/com/msmaker/os/service/TecnicoService.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.msmaker.os.domain.Tecnico;
 import com.msmaker.os.repositories.TecnicoRepository;
+import com.msmaker.os.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class TecnicoService {
 
 	@Autowired
 	private TecnicoRepository repository;
-	
+
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return  obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Tecnico.class.getName()));
 	}
 }
